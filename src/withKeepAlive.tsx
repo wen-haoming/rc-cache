dfa9efe69a9f4686a148416ba4da0ca86eb71e64
wenhaoming <435203093@qq.com>
2021-06-24 00:23:51 +0800

    fix:修复scroll 的问题

--- src/withKeepAlive.tsx
+++ src/withKeepAlive.tsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useRef } from 'react';
+import { ReactEventHandler } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import type { ContextType } from './context';
 import context from './context';
@@ -14,35 +15,43 @@ function withKeepAlive<T extends Record<string, unknown>>(
   const { scroll } = options || {};
   const cacheId = uuidv4();
 
-
-
   return function (props: T) {
     const { contextState, mount, dispatch, handleScroll } = useContext<ContextType>(context);
+
     const ref = useRef<HTMLElement>(null);
+
     useEffect(() => {
+      const scrollEvent = (event: HTMLElementEventMap['scroll']) => handleScroll({ cacheId, event });
       if (scroll) {
-        ref.current?.addEventListener('scroll', (event) => handleScroll({ cacheId, event }),true);
+        ref.current?.addEventListener('scroll', scrollEvent, true);
       }
-    }, []);
+      return () => {
+        if (scroll) {
+          ref.current?.removeEventListener('scroll', scrollEvent);
+        }
+      };
+    }, [handleScroll]);
 
     useEffect(() => {
       const currentState = contextState[cacheId];
       if (currentState && currentState.doms) {
         const { doms } = currentState;
         doms.forEach((dom) => ref.current!.appendChild(dom));
-        if(scroll){
-          doms.forEach(dom=>{
-            if(currentState.scrolls.get(dom)){
-                dom.scrollTop = currentState.scrolls.get(dom);
+        if (scroll) {
+          doms.forEach((dom) => {
+            if (currentState.scrolls.get(dom)) {
+              dom.scrollTop = currentState.scrolls.get(dom);
             }
-          })
+          });
         }
       } else {
         mount({ cacheId, element: <OldComponent {...props} dispatch={dispatch} /> });
       }
-    }, [contextState, dispatch, mount, props,scroll]);
+    }, [contextState, dispatch, mount, props, scroll]);
 
-    return <div ref={ref as any} id={`keep-alive-${cacheId}`} />;
+  
+
+    return <div ref={ref as any}  id={`keep-alive-${cacheId}`} />;
   };
 }
 
